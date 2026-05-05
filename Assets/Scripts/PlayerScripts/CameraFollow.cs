42d2364116d187dcf00cfdb08277ecf8afb8e994
Gonzalo M.C. <gonzalomartca@gmail.com>
2026-05-05 16:20:49 +0200

    Mejoras a las animaciones y cámara
    Añadida animación de roll, mejorado el sistema de animaciones del proyecto y añadido el sistema de delay en el seguimiento de la cámara.
    
    gracias Saif por el mapa :D

--- Assets/Scripts/PlayerScripts/CameraFollow.cs
+++ Assets/Scripts/PlayerScripts/CameraFollow.cs
@@ -2,27 +2,80 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour
+public class CameraController : MonoBehaviour
 {
+    public static CameraController instance;
 
-    Vector3 main;
-    float xMain;
-    float yMain;
-    // Start is called before the first frame update
-    void Start()
+    [Header("Movimiento")]
+    public bool followPlayer;
+    public float velocidadCamara = 3.5f;
+    public Vector3 desplazamiento;
+
+    [Header("Limites")]
+    public Vector2 limiteMin; // abajo-izquierda
+    public Vector2 limiteMax; // arriba-derecha
+
+    private void Awake()
     {
-       
+        instance = this;
+        followPlayer = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
-        main = GameObject.Find("Player").transform.position;
+        if (!followPlayer)
+            return;
 
-        xMain = main.x;
-    
-        yMain = main.y;
-    
-        this.transform.position = new Vector3(xMain, yMain, -15);
+        Vector3 posicionDeseada = PlayerMovement.instance.transform.position + desplazamiento;
+
+
+        // Clamp con los limites
+        posicionDeseada.x = Mathf.Clamp(
+            posicionDeseada.x,
+            limiteMin.x,
+            limiteMax.x
+        );
+
+        posicionDeseada.y = Mathf.Clamp(
+            posicionDeseada.y,
+            limiteMin.y,
+            limiteMax.y
+        );
+
+        posicionDeseada.z = transform.position.z;
+
+        // Suavizado
+        transform.position = Vector3.Lerp(
+            transform.position,
+            posicionDeseada,
+            velocidadCamara * Time.deltaTime
+        );
     }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+
+        Camera cam = GetComponent<Camera>();
+
+        // Tamaño real de la cámara
+        float halfHeight = cam.orthographicSize;
+        float halfWidth = halfHeight * cam.aspect;
+
+        Vector2 camLimitsMin = new Vector2(limiteMin.x - halfWidth, limiteMin.y - halfHeight);
+        Vector2 camLimitsMax = new Vector2(limiteMax.x + halfWidth, limiteMax.y + halfHeight);
+        float xDist = Mathf.Abs(camLimitsMin.x) + Mathf.Abs(camLimitsMax.x);
+        float yDist = Mathf.Abs(camLimitsMin.y) + Mathf.Abs(camLimitsMax.y);
+
+
+        Gizmos.color = Color.red;
+        //Esquina minima
+        Gizmos.DrawRay(camLimitsMin, Vector2.right * xDist);
+        Gizmos.DrawRay(camLimitsMin, Vector2.up * yDist);
+
+        //Esquina maxima
+        Gizmos.DrawRay(camLimitsMax, Vector2.left * xDist);
+        Gizmos.DrawRay(camLimitsMax, Vector2.down * yDist);
+    }
+#endif
 }
